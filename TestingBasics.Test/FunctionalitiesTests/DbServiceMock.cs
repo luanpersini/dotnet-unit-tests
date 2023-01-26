d89779c0ba9f60100b3386e1c89a9e8002011eaf
Luan Persini <luanpersini@hotmail.com>
2023-01-25 23:03:56 -0300

    unit tests

---
+++ TestingBasics.Test/FunctionalitiesTests/DbServiceMock.cs
@@ -0,0 +1,24 @@
+﻿using TestingBasics.Functionalities;
+
+namespace TestingBasics.Test.FunctionalitiesTests;
+
+//Manual mock for DbService
+public class DbServiceMock : IDbService
+{
+    public bool ProcessResult { get; set; }
+    public Product ProductBeingProcessed { get; set; }
+    public int ProductIdBeingProcessed { get; set; }
+
+    public bool RemoveShoppingCartItem(int? id)
+    {
+        if (id != null)
+            ProductIdBeingProcessed = Convert.ToInt32(id);
+        return ProcessResult;
+    }
+
+    public bool SaveShoppingCartItem(Product prod)
+    {
+        ProductBeingProcessed = prod;
+        return ProcessResult;
+    }
+}
---
+++ TestingBasics.Test/FunctionalitiesTests/ShoppingCartTest.cs
@@ -0,0 +1,90 @@
+﻿using Moq;
+using TestingBasics.Functionalities;
+
+namespace TestingBasics.Test.FunctionalitiesTests;
+
+
+public class ShoppingCartTest
+{
+    public readonly Mock<IDbService> _dbServiceMock = new();
+
+    [Fact]
+    public void AddProduct_Success()
+    {
+        var product = new Product(1, "Shoes", 200);
+        _dbServiceMock.Setup(x => x.SaveShoppingCartItem(product)).Returns(true);
+        // Arrange
+        ShoppingCart shoppingCart = new(_dbServiceMock.Object);
+
+        // Act
+        var result = shoppingCart.AddProduct(product);
+
+        // Assert
+        Assert.True(result);
+        _dbServiceMock.Verify(x => x.SaveShoppingCartItem(It.IsAny<Product>()), Times.Once);
+    }
+
+    [Fact]
+    public void AddProduct_Failure_InvalidPayload()
+    {
+
+        // Arrange
+        ShoppingCart shoppingCart = new(_dbServiceMock.Object);
+
+        // Act
+        var result = shoppingCart.AddProduct(null);
+
+        // Assert
+        Assert.False(result);
+        _dbServiceMock.Verify(x => x.SaveShoppingCartItem(It.IsAny<Product>()), Times.Never);
+    }
+
+    [Fact]
+    public void RemoveProduct_Success()
+    {
+        var product = new Product(1, "Shoes", 200);
+        _dbServiceMock.Setup(x => x.RemoveShoppingCartItem(product.Id)).Returns(true);
+
+        // Arrange
+        ShoppingCart shoppingCart = new(_dbServiceMock.Object);
+
+        // Act
+        var result = shoppingCart.DeleteProduct(product.Id);
+
+        // Assert
+        Assert.True(result);
+        _dbServiceMock.Verify(x => x.RemoveShoppingCartItem(It.IsAny<int>()), Times.Once);
+    }
+
+    [Fact]
+    public void RemoveProduct_Failed()
+    {
+        _dbServiceMock.Setup(x => x.RemoveShoppingCartItem(null)).Returns(false);
+
+        // Arrange
+        ShoppingCart shoppingCart = new(_dbServiceMock.Object);
+
+        // Act
+        var result = shoppingCart.DeleteProduct(null);
+
+        // Assert
+        Assert.False(result);
+        _dbServiceMock.Verify(x => x.RemoveShoppingCartItem(null), Times.Never);
+    }
+
+    [Fact]
+    public void RemoveProduct_Failed_InvalidId()
+    {
+        _dbServiceMock.Setup(x => x.RemoveShoppingCartItem(null)).Returns(false);
+
+        // Arrange
+        ShoppingCart shoppingCart = new(_dbServiceMock.Object);
+
+        // Act
+        var result = shoppingCart.DeleteProduct(0);
+
+        // Assert
+        Assert.False(result);
+        _dbServiceMock.Verify(x => x.RemoveShoppingCartItem(null), Times.Never);        
+    }
+}
